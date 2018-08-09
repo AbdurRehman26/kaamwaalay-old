bbcd7b4615c8aa7865dfa95cd667cbaad5ff55bb
Muhammad Noman Sheikh <noman.dildar@cygnismedia.com>
2018-08-09 18:11:22 +0500

    changes

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,7 +5,6 @@ require('./bootstrap');
 window.Vue = require('vue');
 
 import Vue from 'vue';
-import VueRouter from 'vue-router';
 import vbclass from 'vue-body-class';
 import BootstrapVue from 'bootstrap-vue';
 import VueAxios from 'vue-axios'
@@ -88,8 +87,37 @@ Vue.mixin({
 
 const app = new Vue({
     el: '#app',
-    router,
+    router: router,
     store,
+    methods:{
+        //if scroll exist add class else remove class
+        checkscroll(){
+          setTimeout(function(){
+              if(jQuery('body').height() > jQuery(window).height()){
+                    jQuery('body').addClass('handle-scroll');
+                }else{
+                    jQuery('body').removeClass('handle-scroll');
+                }
+          },1500);
+        },
+        //add differnt classes acording to browser
+        browserfunction() {
+            if ((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1) {
+                jQuery('body').addClass('opera-browser')
+            } else if (navigator.userAgent.indexOf("Chrome") != -1) {
+                jQuery('body').addClass('chrome-browser')
+            } else if (navigator.userAgent.indexOf("Safari") != -1) {
+                jQuery('body').addClass('safari-browser')
+            } else if (navigator.userAgent.indexOf("Firefox") != -1) {
+                jQuery('body').addClass('firefox-browser')
+            } else if ((navigator.userAgent.indexOf("MSIE") != -1) || (!!document.documentMode == true)) //IF IE > 10
+            {
+                jQuery('body').addClass('IE-browser')
+            } else {
+                jQuery('body').addClass('New-browser')
+            }
+        }
+    },
     mounted () {
         this.$Progress.finish();
         this.checkscroll();
