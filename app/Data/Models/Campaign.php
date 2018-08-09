fa6e14fc0263bb04f71e9bee04bf89b5209ba216
Rafay Ahmed <rafay@cygnismedia.com>
2018-08-09 23:11:16 +0500

    campaign api completed

--- app/Data/Models/Campaign.php
+++ app/Data/Models/Campaign.php
@@ -3,8 +3,12 @@
 namespace App\Data\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Data\Models\Plan;
 
 class Campaign extends Model
 {
-    //
+	public function plan()
+	{
+		return $this->belongsTo(Plan::class);
+	}
 }
