124d0f623cc986b1fd9cbeb2dd7c337ffca57034
Syed Waqas Imam <syed.waqas@cygnismedia.com>
2018-08-13 11:27:00 +0500

    add  Soft Delete

--- app/Data/Models/Service.php
+++ app/Data/Models/Service.php
@@ -3,11 +3,12 @@
 namespace App\Data\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Service extends Model
 {
     //
-    
+    use SoftDeletes;
     /**
      * The attributes that should be cast to native types.
      *
