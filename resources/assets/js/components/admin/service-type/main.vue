c821b10f5e9f23e908feb27cbfa4fcd2410f7bf8
AFTAB-PC\Ali <ali.kamal@cygnsimedia.com>
2018-08-13 18:18:05 +0500

    service frontend validation done

--- resources/assets/js/components/admin/service-type/main.vue
+++ resources/assets/js/components/admin/service-type/main.vue
@@ -1,88 +1,88 @@
 <template>
-<div>
-	<div class="panel-inner">
+    <div>
+       <div class="panel-inner">
         <div class="row">
             <div class=" col-xs-12 col-md-12">
-                    <div class="datepicker-row">
-                        <div class="row">
-                            <div class="col-xs-12 col-md-3 datepicker-field">
-                              <div class="form-group">
-                                <SearchField></SearchField>
-                              </div>
-                            </div>
-                           <div class="col-xs-12 col-md-2 datepicker-field">
-                              <div class="form-group">
-                                   <label>Is Featured</label>
-                                   <select class="form-control">
-                                     <option>Both</option>
-                                     <option>Yes</option>
-                                     <option>No</option>
-                                   </select>
-                              </div>
-                            </div>                            
-                            <div class="col-xs-12 col-md-2">
-                                <button class="btn btn-primary filter-btn-top-space">
-                                    <span>Apply</span>
-                                    <loader></loader>
-                                </button>
-                            </div>
-                         <div class="float-right add-btn-pos">
-                            <a href="javascript:;" class="btn btn-primary" @click="AddService">Add Service</a>
-                        </div>
+                <div class="datepicker-row">
+                    <div class="row">
+                        <div class="col-xs-12 col-md-3 datepicker-field">
+                          <div class="form-group">
+                            <SearchField></SearchField>
                         </div>
                     </div>
+                    <div class="col-xs-12 col-md-2 datepicker-field">
+                      <div class="form-group">
+                         <label>Is Featured</label>
+                         <select class="form-control">
+                           <option>Both</option>
+                           <option>Yes</option>
+                           <option>No</option>
+                       </select>
+                   </div>
+               </div>                            
+               <div class="col-xs-12 col-md-2">
+                <button class="btn btn-primary filter-btn-top-space">
+                    <span>Apply</span>
+                    <loader></loader>
+                </button>
+            </div>
+            <div class="float-right add-btn-pos">
+                <a href="javascript:;" class="btn btn-primary" @click="AddService">Add Service</a>
             </div>
         </div>
-		<div class="row">
-				<div class="col-md-12">
-					<div class="table-area">
-                        <div class="table-responsive">
-                            <table class="table first-last-col-fix">
-                              <thead>
-                                <tr>
-                                  <th>Id</th>
-                                  <th>Service</th>
-                                  <th>Sub Service</th>
-                                  <th class="text-center">Is Featured</th>
-                                  <th class="text-center">Hero Navigation</th>
-                                  <th class="text-center">Actions</th>
-                                </tr>
-                              </thead>
-                              <tbody>
-                                <tr v-for="list in listing">
-                                  <td>{{list.id}}</th>
-                                  <td>{{list.service}}</td>
-                                  <td>{{list.subservice}}</td>
-                                  <td class="text-center">{{list.featured}}</td>
-                                  <td class="text-center">{{list.heronavigation}}</td>
-                                  <td class="text-center">
-                                    <div class="action-icons">
-                                      <i v-b-tooltip.hover title="View Details" @click="ViewDetails" class="icon-eye"></i>
-                                      <i v-b-tooltip.hover title="Edit Details" class="icon-pencil" @click="AddService"></i>
-                                      <i v-b-tooltip.hover title="Delete" @click="ActionDelete" class="icon-delete"></i>
-                                    </div>
-                                  </td>
-                                </tr>
-                              </tbody>
-                            </table>
-                        </div>
-		    		</div>
-		  		</div>
-		    </div>
-        <div class="row">
-          <div class="col-xs-12 col-md-12">
+    </div>
+</div>
+</div>
+<div class="row">
+    <div class="col-md-12">
+       <div class="table-area">
+        <div class="table-responsive">
+            <table class="table first-last-col-fix">
+              <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>Service</th>
+                  <th>Sub Service</th>
+                  <th class="text-center">Is Featured</th>
+                  <th class="text-center">Hero Navigation</th>
+                  <th class="text-center">Actions</th>
+              </tr>
+          </thead>
+          <tbody>
+            <tr v-for="list in listing">
+              <td>{{list.id}}</th>
+                  <td>{{list.service}}</td>
+                  <td>{{list.subservice}}</td>
+                  <td class="text-center">{{list.featured}}</td>
+                  <td class="text-center">{{list.heronavigation}}</td>
+                  <td class="text-center">
+                    <div class="action-icons">
+                      <i v-b-tooltip.hover title="View Details" @click="ViewDetails" class="icon-eye"></i>
+                      <i v-b-tooltip.hover title="Edit Details" class="icon-pencil" @click="AddService"></i>
+                      <i v-b-tooltip.hover title="Delete" @click="ActionDelete" class="icon-delete"></i>
+                  </div>
+              </td>
+          </tr>
+      </tbody>
+  </table>
+</div>
+</div>
+</div>
+</div>
+<div class="row">
+  <div class="col-xs-12 col-md-12">
 
-            <div class="total-record float-left">
-                <p><strong>Total records: <span>10</span></strong></p>
-            </div>
+    <div class="total-record float-left">
+        <p><strong>Total records: <span>10</span></strong></p>
+    </div>
 
-            <div class="pagination-wrapper float-right">
-                <b-pagination size="md" :total-rows="100" v-model="currentPage" :per-page="10"></b-pagination>
-            </div>
-          </div>
-        </div>
+    <div class="pagination-wrapper float-right">
+        <b-pagination size="md" :total-rows="100" v-model="currentPage" :per-page="10"></b-pagination>
+    </div>
+</div>
+</div>
 
-	</div>
+</div>
 <add-service @HideModalValue="HideModal" :showModalProp="service"></add-service>
 <view-details @HideModalValue="HideModal" :showModalProp="viewdetails"></view-details>
 <delete-popup @HideModalValue="HideModal" :showModalProp="actiondelete"></delete-popup>
@@ -90,136 +90,137 @@
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-    	service: false,
-      viewdetails: false,
-      actiondelete: false,
-        viewcustomer: false,
-            listing: [
-                {
-                    id : 1,
-                    imagepath:'',
-                    service:'Outdoors',
-                    subservice:'Metal Chair',
-                    featured:'No',
-                    heronavigation:'Yes',
-                    description: '',
-                    url: 'www.psm.com',
-                },
-                {
-                    id : 2,
-                    imagepath:'',
-                    service:'Clothing',
-                    subservice:'Stitch',
-                    featured:'Yes',
-                    heronavigation:'Yes',
-                    description: '',
-                    url: 'www.psm.com/clothing/stitch',
-                },
-                {
-                    id : 3,
-                    imagepath:'',
-                    service:'Cleaning Services',
-                    subservice:'',
-                    featured:'No',
-                    heronavigation:'No',
-                    description: 'Testing',
-                    url: 'www.psm.com',
-                },
-                {
-                    id : 4,
-                    imagepath:'',
-                    service:'Carpenters',
-                    subservice:'General Carpenters',
-                    featured:'No',
-                    heronavigation:'yes',
-                    description: '',
-                    url: 'www.psm.com',
-                },
-                {
-                    id : 5,
-                    imagepath:'',
-                    service:'Movers',
-                    subservice:'DHL',
-                    featured:'Yes',
-                    heronavigation:'Yes',
-                    description: '',
-                    url: 'www.psm.com',
-                },
-                {
-                    id : 6,
-                    imagepath:'',
-                    service:'Outdoors',
-                    subservice:'Metal Chair',
-                    featured:'No',
-                    heronavigation:'Yes',
-                    description: '',
-                    url: 'www.psm.com',
-                },
-                {
-                    id : 7,
-                    imagepath:'',
-                    service:'Clothing',
-                    subservice:'Stitch',
-                    featured:'Yes',
-                    heronavigation:'Yes',
-                    description: '',
-                    url: 'www.psm.com/clothing/stitch',
-                },
-                {
-                    id : 8,
-                    imagepath:'',
-                    service:'Cleaning Services',
-                    subservice:'',
-                    featured:'No',
-                    heronavigation:'No',
-                    description: 'Testing',
-                    url: 'www.psm.com',
-                },
-                {
-                    id : 9,
-                    imagepath:'',
-                    service:'Carpenters',
-                    subservice:'General Carpenters',
-                    featured:'No',
-                    heronavigation:'yes',
-                    description: '',
-                    url: 'www.psm.com',
-                },
-                {
-                    id : 10,
-                    imagepath:'',
-                    service:'Movers',
-                    subservice:'DHL',
-                    featured:'Yes',
-                    heronavigation:'Yes',
-                    description: '',
-                    url: 'www.psm.com',
-                },
-            ],
-      }
+    export default {
+      data () {
+        return {
+           service: false,
+           viewdetails: false,
+           actiondelete: false,
+           viewcustomer: false,
+           currentPage: 1,
+           listing: [
+           {
+            id : 1,
+            imagepath:'',
+            service:'Outdoors',
+            subservice:'Metal Chair',
+            featured:'No',
+            heronavigation:'Yes',
+            description: '',
+            url: 'www.psm.com',
+        },
+        {
+            id : 2,
+            imagepath:'',
+            service:'Clothing',
+            subservice:'Stitch',
+            featured:'Yes',
+            heronavigation:'Yes',
+            description: '',
+            url: 'www.psm.com/clothing/stitch',
+        },
+        {
+            id : 3,
+            imagepath:'',
+            service:'Cleaning Services',
+            subservice:'',
+            featured:'No',
+            heronavigation:'No',
+            description: 'Testing',
+            url: 'www.psm.com',
+        },
+        {
+            id : 4,
+            imagepath:'',
+            service:'Carpenters',
+            subservice:'General Carpenters',
+            featured:'No',
+            heronavigation:'yes',
+            description: '',
+            url: 'www.psm.com',
+        },
+        {
+            id : 5,
+            imagepath:'',
+            service:'Movers',
+            subservice:'DHL',
+            featured:'Yes',
+            heronavigation:'Yes',
+            description: '',
+            url: 'www.psm.com',
+        },
+        {
+            id : 6,
+            imagepath:'',
+            service:'Outdoors',
+            subservice:'Metal Chair',
+            featured:'No',
+            heronavigation:'Yes',
+            description: '',
+            url: 'www.psm.com',
+        },
+        {
+            id : 7,
+            imagepath:'',
+            service:'Clothing',
+            subservice:'Stitch',
+            featured:'Yes',
+            heronavigation:'Yes',
+            description: '',
+            url: 'www.psm.com/clothing/stitch',
+        },
+        {
+            id : 8,
+            imagepath:'',
+            service:'Cleaning Services',
+            subservice:'',
+            featured:'No',
+            heronavigation:'No',
+            description: 'Testing',
+            url: 'www.psm.com',
+        },
+        {
+            id : 9,
+            imagepath:'',
+            service:'Carpenters',
+            subservice:'General Carpenters',
+            featured:'No',
+            heronavigation:'yes',
+            description: '',
+            url: 'www.psm.com',
+        },
+        {
+            id : 10,
+            imagepath:'',
+            service:'Movers',
+            subservice:'DHL',
+            featured:'Yes',
+            heronavigation:'Yes',
+            description: '',
+            url: 'www.psm.com',
+        },
+        ],
+    }
+},
+methods: {
+    AddService(){
+        this.service = true;
     },
-    methods: {
-        AddService(){
-            this.service = true;
-        },
-        ViewDetails() {
-            this.viewdetails = true;
-        },
-        ActionDelete() {
-            this.actiondelete = true;
-        },
-        HideModal(){
-            this.service = false;
-            this.viewdetails = false;
-            this.actiondelete = false;
-        },
-        getResponse: function (response) {
-          return response.data.items
-        }
+    ViewDetails() {
+        this.viewdetails = true;
     },
+    ActionDelete() {
+        this.actiondelete = true;
+    },
+    HideModal(){
+        this.service = false;
+        this.viewdetails = false;
+        this.actiondelete = false;
+    },
+    getResponse: function (response) {
+      return response.data.items
+  }
+},
 
 /*    mounted(){
         for (var i = 1; i <= 10; i++) {
--- resources/assets/js/components/admin/service-type/popup/AddService.vue
+++ resources/assets/js/components/admin/service-type/popup/AddService.vue
@@ -1,107 +1,149 @@
  <template>
-	<div>
-		<b-modal id="add-new-service" centered  @hidden="onHidden" title-tag="h4" ok-variant="primary" ref="myModalRef" size="md" title="Add new Service" ok-only ok-title="Submit">
-            <alert></alert>
-		    	<div>
-                    <form action="" method="">
-                    <div class="form-group">
-                    	<label>Parent Service</label>
-                        <select class="form-control">
-                            <option value="" selected="" disabled="">Select Parent Service</option>
-                            <option value="">None</option>
-                            <option value="">Electrician</option>
-                            <option value="">Carpenter</option>
-                            <option value="">Plumber</option>
-                            <option value="">Painter</option>
-                        </select>
-                    </div>
+   <div>
+      <b-modal id="add-new-service" centered  @hidden="onHidden" title-tag="h4" ok-variant="primary" ref="myModalRef" size="md" title="Add new Service" >
+        <alert v-if="errorMessage || successMessage" :errorMessage="errorMessage" :successMessage="successMessage"></alert>
+        <div>
+            <form action="" method="">
+                <div class="form-group">
+                   <label>Parent Service</label>
+                   <select class="form-control" v-model="formData.parentService">
+                    <option value="" selected="" disabled="">Select Parent Service</option>
+                    <option value="">None</option>
+                    <option value="">Electrician</option>
+                    <option value="">Carpenter</option>
+                    <option value="">Plumber</option>
+                    <option value="">Painter</option>
+                </select>
+            </div>
 
-                    <div class="form-group">
-                		<label>Service Name</label>
-                        <input type="text" name="" class="form-control" placeholder="Enter service name">
-                    </div>
+            <div class="form-group">
+              <label>Service Name</label>
+              <input type="text" name="service name" v-validate="'required'" class="form-control" placeholder="Enter service name" v-model="formData.serviceName" :class="['form-group' , errorBag.first('service name') ? 'is-invalid' : '']" >
+          </div>
 
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-6 col-md-6">
-                            <div class="form-group radio-group-row">
-                                <label>Is Featured?</label>
-                                    <div class="form-check form-check-inline">
-                                      <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
-                                      <label class="form-check-label" for="inlineRadio1">Yes</label>
-                                    </div>
-                                    <div class="form-check form-check-inline">
-                                      <input checked=""  class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
-                                      <label class="form-check-label" for="inlineRadio1">No</label>
-                                    </div>
-                            </div>
-                        </div>
-                        <div class="col-xs-12 col-sm-6 col-md-6">
-                            <div class="form-group radio-group-row">
-                                <label>Hero Navigation?</label>
-                                    <div class="form-check form-check-inline">
-                                      <input class="form-check-input" type="radio" name="inlineRadioOptions2" id="inlineRadio2" value="option1">
-                                      <label class="form-check-label" for="inlineRadio2">Yes</label>
-                                    </div>
-                                    <div class="form-check form-check-inline">
-                                      <input checked=""  class="form-check-input" type="radio" name="inlineRadioOptions2" id="inlineRadio2" value="option1">
-                                      <label class="form-check-label" for="inlineRadio2">No</label>
-                                    </div>
-                            </div>
-                        </div>
-                    </div>
+          <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-6">
+                <div class="form-group radio-group-row">
+                    <label>Is Featured?</label>
+                    <div class="form-check form-check-inline">
+                      <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="1" v-model="formData.isFeatured">
+                      <label class="form-check-label" for="inlineRadio1">Yes</label>
+                  </div>
+                  <div class="form-check form-check-inline">
+                      <input checked=""  class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="0" v-model="formData.isFeatured">
+                      <label class="form-check-label" for="inlineRadio1">No</label>
+                  </div>
+              </div>
+          </div>
+          <div class="col-xs-12 col-sm-6 col-md-6">
+            <div class="form-group radio-group-row">
+                <label>Hero Navigation?</label>
+                <div class="form-check form-check-inline">
+                  <input class="form-check-input" type="radio" name="inlineRadioOptions2" id="inlineRadio2" value="1" v-model="formData.isHeroNavigation">
+                  <label class="form-check-label" for="inlineRadio2">Yes</label>
+              </div>
+              <div class="form-check form-check-inline">
+                  <input checked=""  class="form-check-input" type="radio" name="inlineRadioOptions2" id="inlineRadio2" value="0" v-model="formData.isHeroNavigation">
+                  <label class="form-check-label" for="inlineRadio2">No</label>
+              </div>
+          </div>
+      </div>
+  </div>
 
-                    <div class="form-group">
-                        <label>Description</label>
-                        <textarea class="form-control" rows="5" placeholder="Enter description"></textarea>
-                    </div>
+  <div class="form-group">
+    <label>Description</label>
+    <textarea class="form-control" rows="5" placeholder="Enter description" v-model="formData.serviceDescription" name="description" v-validate="'required'" :class="['form-group' , errorBag.first('description') ? 'is-invalid' : '']"></textarea>
+</div>
 
-                    <div class="form-group">
-                        <label>Upload Image</label>
-                         <b-form-file @change="onFileChange" v-model="file" accept=".jpg, .png" placeholder="Click here to upload image"></b-form-file>
-                        <div class="uploded-picture">
-                            <img :src="image" />
-                        </div>
-                    </div>
+<div class="form-group">
+    <label>Upload Image</label>
+    <b-form-file @change="onFileChange" v-model="file" accept=".jpg, .png" placeholder="Click here to upload image" name="upload image" v-validate="'required'"  :class="['form-group' , errorBag.first('upload image') ? 'is-invalid' : '']"></b-form-file>
+    <div class="uploded-picture">
+        <img :src="image" />
+    </div>
+</div>
 
-                    <div class="form-group">
-                        <label>URL</label>
-                        <input type="text" class="form-control" placeholder="Enter url" name="">
-                    </div>
-                    </form>
+<div class="form-group">
+    <label>URL</label>
+    <input type="text" class="form-control" placeholder="Enter url" name="" v-model="formData.urlPrefix" name="url" v-validate="'required|url'" :class="['form-group' , errorBag.first('url') ? 'is-invalid' : '']">
+</div>
+</form>
 
-		    	</div>
-	    </b-modal>
-	</div>
+</div>
+<div slot="modal-footer" class="">
+    <b-col class="float-right" cols="12">
+     <button :class="[loading  ? 'show-spinner' : '' , 'btn' , 'btn-primary' , 'apply-primary-color' ]" @click.prevant="validateBeforeSubmit">
+        <span>Submit</span> 
+        <loader></loader>
+    </button>
+</button>
+</b-col>
+</div>
+</b-modal>
+</div>
 </template>
 
 <script>
-export default {
-    data () {
-        return {
-            emailaddress: 'arsalan@cygnismedia.com',
-            fullname: 'Arsalan Akhtar',
-            image: 'images/dummy/user-pic.jpg',
-            file: null,
-        }
-    },
+    export default {
+        data () {
+            return {
 
-    props: ['showModalProp'],
+                errorMessage: '',
+                successMessage: '',
+                formData: {
+                    parentService: '',
+                    serviceName: '',
+                    serviceDescription: '',
+                    isFeatured: 0,
+                    isHeroNavigation: 0,
+                    images: [
+                    {
+                        'name': '', 
+                        'original_name': ''
+                    }
+                    ],
+                    urlPrefix: '',
+                    status: 1,
+                    isDisplayBanner: 1,
+                    isDisplayServiceNav: 1,
+                    isDisplayFooterNav: 1,
 
-    methods: {
-        showModal () {
-            this.$refs.myModalRef.show()
+                },
+                emailaddress: 'arsalan@cygnismedia.com',
+                fullname: 'Arsalan Akhtar',
+                image: 'images/dummy/user-pic.jpg',
+                file: null,
+            }
         },
-        hideModal () {
-            this.$refs.myModalRef.hide()
-        },
-        onHidden() {
-            this.$emit('HideModalValue');
-        },
-        onFileChange(e) {
-          var files = e.target.files || e.dataTransfer.files;
-          if (!files.length)
-            return;
-          this.createImage(files[0]);
+
+        props: ['showModalProp'],
+
+        methods: {
+            validateBeforeSubmit() {
+                console.log(this.formData);
+                this.$validator.validateAll().then((result) => {
+                    if (result) {
+                        this.onSubmit();
+                        this.errorMessage = '';
+                        return;
+                    }
+                    this.errorMessage = this.errorBag.all()[0];
+                });
+            },  
+            showModal () {
+                this.$refs.myModalRef.show()
+            },
+            hideModal () {
+                this.$refs.myModalRef.hide()
+            },
+            onHidden() {
+                this.$emit('HideModalValue');
+            },
+            onFileChange(e) {
+              var files = e.target.files || e.dataTransfer.files;
+              if (!files.length)
+                return;
+            this.createImage(files[0]);
         },
         createImage(file) {
           var image = new Image();
@@ -110,22 +152,23 @@ export default {
 
           reader.onload = (e) => {
             vm.image = e.target.result;
-          };
-          reader.readAsDataURL(file);
-        },
-
+        };
+        reader.readAsDataURL(file);
     },
+    onSubmit() {
+        console.log(this.formData);
+    }
+},
 
-    watch: {
-        showModalProp(value) {
-
-            if(value) {
-                this.showModal();
-            }
-            if(!value) {
-                this.hideModal();
-            }
+watch: {
+    showModalProp(value) {
+        if(value) {
+            this.showModal();
         }
-    },
+        if(!value) {
+            this.hideModal();
+        }
+    }
+},
 }
 </script>
