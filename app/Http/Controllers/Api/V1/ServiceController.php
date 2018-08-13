6e9bfc8d30324744a6af3881ecb7df6b3fb2654a
Syed Waqas Imam <syed.waqas@cygnismedia.com>
2018-08-13 15:52:02 +0500

    service parent id

--- app/Http/Controllers/Api/V1/ServiceController.php
+++ app/Http/Controllers/Api/V1/ServiceController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Data\Repositories\ServiceRepository;
 use App\Data\Models\Role;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ServiceController extends ApiResourceController
 {
@@ -31,8 +34,15 @@ class ServiceController extends ApiResourceController
 
     if($value == 'update'){
         
-        $rules['id'] =  'required|exists:services,id';
-        $rules['user_id'] =  'required|exists:users,id';
+        
+        $rules['id']                      =  'required|exists:services,id';
+        $rules['title']                   = 'required'; 
+        $rules['is_display_banner']       = 'nullable|in:0,1';                   
+        $rules['is_display_service_nav']  = 'nullable|in:0,1';                       
+        $rules['is_display_footer_nav']   = 'nullable|in:0,1';           
+        $rules['status']                  = 'nullable|in:0,1';        
+        $rules['user_id']                 =  'required|exists:users,id';
+
     }
 
 
@@ -78,5 +88,29 @@ class ServiceController extends ApiResourceController
                             );
         $input['user_id'] = !empty(request()->user()->id) ? request()->user()->id : null ;
     return $input;
+    }
+
+
+    //Update single record
+    public function update(Request $request, $id)
+    {   
+
+        $request->request->add(['id' => $id]);
+        $input = $this->input(__FUNCTION__);
+        $rules = $this->rules(__FUNCTION__);
+        $messages = $this->messages(__FUNCTION__);
+        $this->validate($request, $rules, $messages);
+
+        $data = $this->_repository->update($input);
+        
+        if ($data == 'not_parent') {
+            $output = ['errors' => ['parent_id' => ['The parent id does not match']] , 'message' => 'The given data was invalid'];
+        }else{
+            $output = ['response' => ['data' => $data, 'message' => $this->response_messages(__FUNCTION__)]];
+        }
+
+        // HTTP_OK = 200;
+        return response()->json($output, Response::HTTP_OK);
+
     }
 }
