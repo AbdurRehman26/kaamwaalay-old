60b1c1fb76e0eed23294d20267554e563f7999dc
rashid awan <rashid.awan@startrum.com>
2018-08-16 10:28:11 +0500

    all popup is create

--- resources/assets/js/front-components-tags.js
+++ resources/assets/js/front-components-tags.js
@@ -21,6 +21,13 @@ Vue.component('alert', require('./components/front/common-components/Alert.vue')
 Vue.component('change-password-popup', require('./components/admin/common-components/ChangePassPopup.vue'));
 Vue.component('testmonial-sec',require('./components/front/common-components/TestmonialSec.vue'));
 Vue.component('category-popup',require('./components/front/common-components/CateogyPopup.vue'));
+Vue.component('award-job-popup',require('./components/front/common-components/AwardJobPopup.vue'));
+Vue.component('visit-request-popup',require('./components/front/common-components/VisitRequestPopup.vue'));
+Vue.component('post-bid-popup',require('./components/front/common-components/PostBidPopup.vue'));
+Vue.component('customer-support-popup',require('./components/front/common-components/CustomerSupportPopup.vue'));
+Vue.component('go-to-visit-popup',require('./components/front/common-components/GoToVisitPopup.vue'));
+Vue.component('write-review-popup',require('./components/front/common-components/writeReviewPopup.vue'));
+
 
 //header
 Vue.component('front-header',require('./components/front/common-components/header.vue'));
