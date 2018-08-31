f2be5536db064a6cf4f4e70a1f4e333673583c65
AFTAB-PC\Ali <ali.kamal@cygnsimedia.com>
2018-08-31 12:38:55 +0500

    fixed naming conventions & modify parent service column order

--- resources/assets/js/components/admin/service-type/main.vue
+++ resources/assets/js/components/admin/service-type/main.vue
@@ -28,7 +28,7 @@
             </button>
           </div>
           <div class="float-right add-btn-pos">
-            <a href="javascript:;" class="btn btn-primary" @click="AddService">Add Service</a>
+            <a href="javascript:;" class="btn btn-primary" @click="addService">Add Service</a>
           </div>
         </div>
       </div>
@@ -42,51 +42,49 @@
           <thead>
             <tr>
               <th>Service</th>
-              <th>Sub Service</th>
+              <th>Parent Service</th>
               <th class="text-center">Status</th>
               <th class="text-center">Actions</th>
             </tr>
           </thead>
           <tbody>
             <tr v-for="(list, index) in listing" v-if="listing.length && !loadingStart">
-                <td>{{list.parent_id? list.parent.title: list.title}}</td>
-                <td>{{list.parent_id? list.title : list.parent.title }}</td>
-                <td class="text-center"><span class="tags" :class="[list.status? 'completed':'banned']">
-                        {{list.status? "ACTIVE":"INACTIVE"}}
-                    </span></td>
-                <td class="text-center">
-                  <div class="action-icons">
-                    <i v-b-tooltip.hover title="View Details" @click="ViewDetails(list, index)" class="icon-eye"></i>
-                    <i v-b-tooltip.hover title="Edit Details" class="icon-pencil" @click="updateService(list)"></i>
-                    <i @click="changestatuspopup(list)" :class="[list.status === 'pending'  ? 'disabled' : '']" v-b-tooltip.hover title="Change Status" class="icon-cog2"></i>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-          <block-spinner v-if="loadingStart"></block-spinner>
-          <no-record-found v-if="!listing.length && showNoRecordFound"></no-record-found>
-        </div>
+              <td>{{list.title}}</td>
+              <td>{{list.parent_id? list.parent.title : '-'}}</td>
+              <td class="text-center"><span class="tags" :class="[list.status? 'completed':'banned']">
+                {{list.status? "ACTIVE":"INACTIVE"}}
+              </span></td>
+              <td class="text-center">
+                <div class="action-icons">
+                  <i v-b-tooltip.hover title="View Details" @click="showViewDetails(list, index)" class="icon-eye"></i>
+                  <i v-b-tooltip.hover title="Edit Details" class="icon-pencil" @click="updateService(list)"></i>
+                  <i @click="changeStatusPopup(list)" :class="[list.status === 'pending'  ? 'disabled' : '']" v-b-tooltip.hover title="Change Status" class="icon-cog2"></i>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <block-spinner v-if="loadingStart"></block-spinner>
+        <no-record-found v-if="!listing.length && showNoRecordFound"></no-record-found>
       </div>
     </div>
   </div>
-  <div class="row">
-    <div class="col-xs-12 col-md-12">
-
-      <div class="total-record float-left" v-if="pagination.total">
-        <p><strong>Total records: <span>{{pagination.total}}</span></strong></p>
-      </div>
-
-      <div class="pagination-wrapper float-right" v-if="pagination.total">
-        <b-pagination size="md" :total-rows="pagination.total" v-model="currentPage" :per-page="25"></b-pagination>
-      </div>
-    </div>
-  </div>
-
 </div>
-<add-service @HideModalValue="HideModal" :showModalProp="service" @call-list="getList(false, false)" :isUpdate="isUpdate" :list="list"></add-service>
-<view-details @HideModalValue="HideModal" :showModalProp="viewdetails" :selectedService="selectedService"></view-details>
-<changestatuspopup @HideModalValue="HideModal" :showModalProp="changestatus" :statusData="statusData" :options="ChangeStatusesOptions" :url="statusUrl" ></changestatuspopup>
+<div class="row">
+  <div class="col-xs-12 col-md-12">
+    <div class="total-record float-left" v-if="pagination.total">
+      <p><strong>Total records: <span>{{pagination.total}}</span></strong></p>
+    </div>
+
+    <div class="pagination-wrapper float-right" v-if="pagination.total">
+      <b-pagination size="md" :total-rows="pagination.total" v-model="currentPage" :per-page="25"></b-pagination>
+    </div>
+  </div>
+</div>
+</div>
+<add-service @HideModalValue="hideModal" :showModalProp="service" @call-list="getList(false, false)" :isUpdate="isUpdate" :list="list"></add-service>
+<view-details @HideModalValue="hideModal" :showModalProp="viewDetails" :selectedService="selectedService"></view-details>
+<changestatuspopup @HideModalValue="hideModal" :showModalProp="changeStatus" :statusData="statusData" :options="changeStatusOptions" :url="statusUrl" ></changestatuspopup>
 </div>
 </template>
 
@@ -95,9 +93,7 @@
     data () {
       return {
        service: false,
-       viewdetails: false,
-       actiondelete: false,
-       viewcustomer: false,
+       viewDetails: false,
        currentPage: 1,
        listing: [],
        search : '',
@@ -110,10 +106,10 @@
        list: {},
        loading: false,
        loadingStart: true,
-       changestatus: false,
+       changeStatus: false,
        statusData:'',
        statusUrl: 'api/service',
-       ChangeStatusesOptions : [
+       changeStatusOptions : [
        {
         key : 0,
         value : 'Inactive'
@@ -127,21 +123,18 @@
   },
   watch : {
     currentPage(pageNumber){
-
       var data = {
         search : this.search,
         filter: this.filter_by_featured
       };
       this.getList(data, pageNumber);
-
     },
   },
   methods: {
-
-    changestatuspopup(list) {
-      this.statusUrl = 'api/service/'+list.id;
+    changeStatusPopup(list) {
+      this.statusUrl = 'api/service/' + list.id;
       this.statusData = list;
-      this.changestatus = true;
+      this.changeStatus = true;
     },
     onApply() {
       this.loadingStart = true;
@@ -152,7 +145,7 @@
       };
       this.getList(data, false);
     },
-    AddService(){
+    addService(){
       this.isUpdate = false;
       this.service = true;
     },
@@ -161,23 +154,21 @@
       this.isUpdate = true;
       this.service = true;
     },
-    ViewDetails(list, index) {
+    showViewDetails(list, index) {
       list['index'] = index;
       this.selectedService = list;
-      this.viewdetails = true;
+      this.viewDetails = true;
     },
-    HideModal(){
+    hideModal(){
       this.service = false;
-      this.viewdetails = false;
-      this.actiondelete = false;
+      this.viewDetails = false;
       this.isUpdate = false;
-      this.changestatus = false;
+      this.changeStatus = false;
       this.list = {};
     },
     getResponse(response) {
       return response.data.items
     },
-
     getList(data , page , successCallback){
       let self = this;
       self.showNoRecordFound = false;
@@ -215,8 +206,6 @@
         if(!self.listing.length) {
           self.showNoRecordFound = true;
         }
-        var serviceArray = _.filter(self.listing, {parent_id: null});
-        
         self.pagination = response.pagination;
 
         if (!self.listing.length) {
@@ -236,16 +225,6 @@
   },
   mounted(){
     this.getList(false, false);
-        // for (var i = 1; i <= 10; i++) {
-        //     var loopperson =  {
-        //                 id : i,
-        //                 imagepath:'',
-        //                 service: this.$faker().commerce.department(),
-        //                 subservice: this.$faker().commerce.productName(),
-        //     };
-        //     this.listing.push(loopperson);
-        // };
-      },
-
-    }
-  </script>
+  },
+}
+</script>
