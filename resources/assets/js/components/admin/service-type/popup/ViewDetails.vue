237527cef1f9dce6a7caae895f51eccfefb900b6
AFTAB-PC\Ali <ali.kamal@cygnsimedia.com>
2018-08-31 12:50:13 +0500

    fixed naming convention & parent service column

--- resources/assets/js/components/admin/service-type/popup/ViewDetails.vue
+++ resources/assets/js/components/admin/service-type/popup/ViewDetails.vue
@@ -5,26 +5,18 @@
         <div class="view-details-list">
             <b-row>
                 <b-col cols="5" class="">
-                    <p><strong class="title-head">ID</strong></p>
-                </b-col>
-                <b-col cols="7">
-                    <p>{{(selectedService.index + 1)}}</p>
-                </b-col>
-            </b-row>
-            <b-row>
-                <b-col cols="5" class="">
                     <p><strong class="title-head">Service</strong></p>
                 </b-col>
                 <b-col cols="7">
-                    <p>{{selectedService.parent_id? selectedService.parent.title : selectedService.title}}</p>
+                    <p>{{selectedService.title}}</p>
                 </b-col>
             </b-row>
             <b-row>
                 <b-col cols="5" class="">
-                    <p><strong class="title-head">Sub-Service</strong></p>
+                    <p><strong class="title-head">Parent Service</strong></p>
                 </b-col>
                 <b-col cols="7">
-                    <p>{{selectedService.parent_id? selectedService.title : "-"}}</p>
+                    <p>{{selectedService.parent_id? selectedService.parent.title : '-'}}</p>
                 </b-col>
             </b-row>
             <b-row>
@@ -35,14 +27,6 @@
                     <p>{{selectedService.parent_id? selectedService.parent.url_prefix : selectedService.url_prefix}}</p>
                 </b-col>
             </b-row>
-            <!--<b-row>
-                <b-col cols="5" class="">
-                    <p><strong class="title-head">Is Featured?</strong></p>
-                </b-col>
-                <b-col cols="7">
-                    <p>{{selectedService.is_featured? "YES":"NO"}}</p>
-                </b-col>
-            </b-row>-->
             <b-row>
                 <b-col cols="5" class="">
                     <p><strong class="title-head">Home Page Banner</strong></p>
@@ -59,15 +43,6 @@
                     <p>{{selectedService.is_display_service_nav? "YES":"NO"}}</p>
                 </b-col>
             </b-row>
-            <!--<b-row>
-                <b-col cols="5" class="">
-                    <p><strong class="title-head">Display Footer Navigation</strong></p>
-                </b-col>
-                <b-col cols="7">
-                    <p>{{selectedService.is_display_footer_nav? "YES":"NO"}}</p>
-                </b-col>
-            </b-row>-->
-
             <b-row>
                 <b-col cols="5" class="">
                     <p><strong class="title-head">Total Service Providers</strong></p>
@@ -96,7 +71,7 @@
                 <b-col cols="5" class="">
                     <p><strong class="title-head">Description</strong></p>
                 </b-col>
-                <b-col cols="12">
+                <b-col cols="7">
                     <div class="form-group">
                         <p>{{selectedService.parent_id? selectedService.parent.description : selectedService.description}}</p>
                     </div>
@@ -111,48 +86,33 @@
 export default {
     props: ['showModalProp', 'selectedService'],
     data () {
-      return {
-       successMessage: "",
-   }
-},
-methods: {
-    showModal () {
-        this.$refs.myModalRef.show();
-
-    },
-    hideModal () {
-        this.loading = false;
-        this.$refs.myModalRef.hide();
-    },
-    onHidden() {
-        this.$emit('HideModalValue');
-    }
-},
-
-methods: {
-  showModal () {
-   this.$refs.myModalRef.show()
-},
-hideModal () {
-   this.$refs.myModalRef.hide()
-},
-onHidden() {
-   this.$emit('HideModalValue');
-}
-},
-watch: {
-       showModalProp(value) {
-          if(value) {
-            this.showModal();
-        }
-        if(!value) {
-            this.hideModal();
+        return {
+            successMessage: "",
         }
     },
-    selectedService(value) {
-        this.selectedService = value;
+    methods: {
+        showModal () {
+            this.$refs.myModalRef.show();
+        },
+        hideModal () {
+            this.loading = false;
+            this.$refs.myModalRef.hide();
+        },
+        onHidden() {
+            this.$emit('HideModalValue');
+        }
     },
-},
-
+    watch: {
+        showModalProp(value) {
+            if(value) {
+                this.showModal();
+            } else {
+                this.hideModal();
+            }
+        },
+        selectedService(value) {
+            this.selectedService = value;
+        },
+    },
 }
 </script>
