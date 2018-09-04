1b6ea1ddc9121486e069d839d1f6a9c82d1caf5a
Muhammad Noman Sheikh <noman.dildar@cygnismedia.com>
2018-09-04 12:33:22 +0500

    run seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
       
         $this->call(ServiceProviderProfileSeeder::class);
 
+
         \Artisan::call('passport:install');
         \Artisan::call('cache:clear');
     }
