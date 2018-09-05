81f117430429c6ea2a30a2cbd8f6d137cf512e10
Muhammad Noman Sheikh <noman.dildar@cygnismedia.com>
2018-09-05 14:30:36 +0500

    fix payment seeder

--- database/seeds/PaymentTableSeeder.php
+++ database/seeds/PaymentTableSeeder.php
@@ -5,6 +5,7 @@ use App\Data\Models\Plan;
 use App\Data\Models\Payment;
 use Carbon\Carbon;
 use Faker\Factory as Faker;
+use App\Data\Models\Role;
 
 class PaymentTableSeeder extends Seeder
 {
@@ -21,7 +22,6 @@ class PaymentTableSeeder extends Seeder
         $date = Carbon::now();
         $faker = Faker::create();
         $plans = new Plan();
-        $getPlans = $plans->get();
         $data = [];
         $i =1;
 
@@ -29,20 +29,25 @@ class PaymentTableSeeder extends Seeder
         $totalJobs = 20;
 
         $serviceIds = app('ServiceRepository')->model->inRandomOrder()->limit($totalServices)->pluck('id')->toArray();
+        $planIds = app('PlanRepository')->model->inRandomOrder()->limit(10)->pluck('id')->toArray();
         
         $jobIds = app('JobRepository')->model->inRandomOrder()->limit($totalJobs)->pluck('id')->toArray();
-        
+        $customerIds = app('UserRepository')->model->inRandomOrder()->where('role_id','=',Role::CUSTOMER)->limit(10)->pluck('id')->toArray();
+        $serviceProviderIds = app('UserRepository')->model->inRandomOrder()->where('role_id','=',Role::SERVICE_PROVIDER)->limit(10)->pluck('id')->toArray();
+
         if(!empty($jobIds)){
 
-            foreach ($getPlans as $getPlan) {
+            foreach (range(1,150) as $index) {
+                $planId = $planIds[array_rand($planIds)];
+                $plan = $plans->where('id','=',$planId)->first();
                 $data[]=[
                     'id' => $i,
-                    'amount' => $getPlan->amount,
-                    'pay_by' =>$getPlan->id,
+                    'amount' => $plan['amount'],
+                    'pay_by' =>($plan['type'] == 'service')? $serviceProviderIds[array_rand($serviceProviderIds)] :$customerIds[array_rand($customerIds)],
                     'service_id' => $serviceIds[array_rand($serviceIds)],
                     'job_id' => $jobIds[array_rand($jobIds)],
-                    'plan_id' =>$getPlan->id,
-                    'type' => $getPlan->type,
+                    'plan_id' =>$planIds[array_rand($planIds)],
+                    'type' => ($plan['type'] == 'service')?'Featured':'Urgent',
                     'transaction_id' => rand(),
                     'created_at' => $date,
                     'updated_at' => $date,
