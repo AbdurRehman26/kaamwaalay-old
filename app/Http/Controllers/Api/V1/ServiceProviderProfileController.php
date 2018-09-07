49f01c64728c54e49fc4a0b7908ed476b3728b49
Ali <ali.kamal@cygnsimedia.com>
2018-09-07 09:22:50 +0500

    qa checkout

--- app/Http/Controllers/Api/V1/ServiceProviderProfileController.php
+++ app/Http/Controllers/Api/V1/ServiceProviderProfileController.php
@@ -33,7 +33,7 @@ class ServiceProviderProfileController extends ApiResourceController
     $input = request()->only(
         'id', 'pagination', 'keyword',
         'filter_by_business_type', 'filter_by_service',
-        'user_rating', 'zip', 'filter_by_featured', 'is_approved'
+        'user_rating', 'zip', 'filter_by_featured', 'is_approved', 'user_detail', 'is_verified'
     );
 
     if (request()->user()) {
