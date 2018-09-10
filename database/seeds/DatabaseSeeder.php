011aa75534b265bee2a99be6ed34ddc9763856c1
Kazmi <abdur.rehman@startrum.com>
2018-09-10 14:22:40 +0500

    Commenting seeders again

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,32 +15,32 @@ class DatabaseSeeder extends Seeder
 
         // commenting all seeders 
 
-        $this->call(CountriesTableSeeder::class);
-        $this->call(StatesTableSeeder::class);
-        $this->call(CitiesTableSeeder::class);
-        $this->call(ZipCodesTableSeeder::class);
+       //  $this->call(CountriesTableSeeder::class);
+       //  $this->call(StatesTableSeeder::class);
+       //  $this->call(CitiesTableSeeder::class);
+       //  $this->call(ZipCodesTableSeeder::class);
 
-        $this->call(RolesTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
+       //  $this->call(RolesTableSeeder::class);
+       //  $this->call(UsersTableSeeder::class);
         
-       $this->call(SupportQuestionsTableSeeder::class);
-       $this->call(PlansTableSeeder::class);
+       // $this->call(SupportQuestionsTableSeeder::class);
+       // $this->call(PlansTableSeeder::class);
 
-        // Service and Service Provider 
+       //  // Service and Service Provider 
 
-        $this->call(ServiceTableSeeder::class);
-        $this->call(ServiceProviderProfileSeeder::class);
+       //  $this->call(ServiceTableSeeder::class);
+       //  $this->call(ServiceProviderProfileSeeder::class);
 
-        // Job bid and rating commented
+       //  // Job bid and rating commented
         
-        $this->call(JobTableSeeder::class);
-        $this->call(JobBidTableSeeder::class);
+       //  $this->call(JobTableSeeder::class);
+       //  $this->call(JobBidTableSeeder::class);
 
-        // These seeders will be commented
-        $this->call(UserRatingTableSeeder::class);
-        $this->call(PaymentTableSeeder::class);
+       //  // These seeders will be commented
+       //  $this->call(UserRatingTableSeeder::class);
+       //  $this->call(PaymentTableSeeder::class);
 
-        $this->call(SupportInquiryTableSeeder::class);
+       //  $this->call(SupportInquiryTableSeeder::class);
 
         \Artisan::call('passport:install');
         \Artisan::call('cache:clear');
