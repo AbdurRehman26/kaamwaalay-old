1959a13fbb977b3e592ffb75eff5f678b543ea72
Kazmi <abdur.rehman@startrum.com>
2018-09-17 17:04:53 +0500

    ratings table seeder fixed

--- database/seeds/UserRatingTableSeeder.php
+++ database/seeds/UserRatingTableSeeder.php
@@ -16,13 +16,17 @@ class UserRatingTableSeeder extends Seeder
 
         echo "\nThis Seeder requires Jobs and Job Bids to be present in data base.\n"; 
 
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        \DB::table('user_ratings')->truncate();
+
+
         $faker = Faker\Factory::create();
         $now = Carbon::now()->toDateTimeString();
 
 
-        $jobBids = app('JobBidRepository')->model->join('jobs' , 'jobs.id' , 'job_bids.job_id')->where('is_awarded' , 1)->where('job_bids.status' , 'completed')
+        $jobBids = app('JobBidRepository')->model->join('jobs' , 'jobs.id' , 'job_bids.job_id')->where('is_awarded' , 1)->where('jobs.status' , 'completed')
         ->inRandomOrder()
-        ->get(['jobs.user_id' , 'job_bids.id', 'job_bids.id', 'job_bids.job_id', 'job_bids.user_id as service_provider_user_id']);
+        ->get(['jobs.user_id as customer_user_id' , 'job_bids.id', 'job_bids.id', 'job_bids.job_id', 'job_bids.user_id as service_provider_user_id']);
 
         $ratings = [0, 1, 3, 4, 5];
 
@@ -33,7 +37,7 @@ class UserRatingTableSeeder extends Seeder
                 $data[] = [
                     'job_id' => $bid->job_id,
                     'user_id' => $bid->service_provider_user_id,
-                    'rated_by' => $bid->user_id,
+                    'rated_by' => $bid->customer_user_id,
                     'rating' => $ratings[array_rand($ratings)],
                     'status' => 'approved',
                     'message' => $faker->Text,
@@ -43,6 +47,8 @@ class UserRatingTableSeeder extends Seeder
                 ];
 
             }
+
+            DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
             app('UserRatingRepository')->model->insertOnDuplicateKey($data);
 
