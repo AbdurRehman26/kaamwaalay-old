5a28e08acbc5792e8bf8c6334fe4266a60924a44
Ali Kamal <ali.kamal@cygnismedia.com>
2018-09-17 16:21:38 +0500

    fixed related Services collapse

--- resources/assets/js/components/front/explore/service-provider.vue
+++ resources/assets/js/components/front/explore/service-provider.vue
@@ -130,7 +130,7 @@
 							<div>
 							  <!-- element to collapse -->
 							  <a v-b-toggle="service.title" href="javascript:void(0);" class="showCollapse" v-if="getRemainingSubServices(relatedServices).length">View all related services<i class="icon-angle-right"></i></a>
-							  <b-collapse :id="service.title" class="customCollapse">
+							  <b-collapse :id="service.title" v-if="getRemainingSubServices(relatedServices).length">
 							    <b-card>
 							      <div class="items service-remain-category" v-for="remainingSubServices in getRemainingSubServices(relatedServices)">
 									<a @click="changecategorypopup(remainingSubServices)" href="javascript:void(0);">
@@ -327,7 +327,6 @@
 			});
 		},
 		getRelatedServices() {
-			$('.customCollapse').hide();
 			let self = this;
 			let url = 'api/service/?filter_by_related_services=' + this.service.id;
 			self.$http.get(url).then(response => {
