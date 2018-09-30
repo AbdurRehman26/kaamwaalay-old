eb23676ccc7e900adcfdf7e889d9e7e864249893
KAZMI <sydabdrehman@gmail.com>
2018-09-30 16:16:21 +0500

    fixed explore job issue

---
+++ resources/assets/js/components/front/jobs/ExploreJobList.vue
@@ -0,0 +1,42 @@
+<template>
+
+    <div v-if="records.length">
+        <div class="job-post-list" v-for="record in records">
+            <job :job="record" @chatMessage="chatMessage" @showBidPopup="bidPopup"></job>
+        </div>
+        <post-bid-popup :bid="bidValue" :job="currentRecord" @HideModalValue="showBidPopup = false; bidValue = '';" :showModalProp="showBidPopup"></post-bid-popup>
+
+    </div>
+
+</template>
+
+<script>
+    import StarRating from 'vue-star-rating';
+
+    export default{
+        props : ['records'],
+        components: {
+            StarRating
+        },
+        data(){
+            return { 
+                bidValue : '',
+                currentRecord : '',
+                showBidPopup: false,
+            }
+        },
+        methods: {
+            bidPopup(job, val) {
+                if(val == 'bidNow') {
+                    this.currentRecord = job;
+                }else {
+                    this.currentRecord = job;
+                    this.bidValue = job.my_bid;
+                }
+                this.showBidPopup = true;
+            }
+        }
+
+    }
+
+</script>
