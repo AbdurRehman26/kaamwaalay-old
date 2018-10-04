86a6c8c309438a8f3fdfc4fd98293b51cc5d71f8
Bakhtiar Aleem <bakhtiar.aleem@startrum.com>
2018-10-04 12:16:51 +0500

    fixes

--- resources/assets/js/components/front/common-components/ProfileRejection.vue
+++ resources/assets/js/components/front/common-components/ProfileRejection.vue
@@ -1,12 +1,12 @@
 <template>
-	<div class="profile-rejection-box">
+	<div class="profile-rejection-box m-t-50 m-b-50">
 		<div class="rejectionicon">
 			<img class="building" src="images/front/svg/sp-default.svg">
 			<img class="cross" src="images/front/svg/cancel.svg">
 		</div>
 		<h2>Your profile has been rejected.</h2>
-        <router-link to="/apply-for-review" class="btn btn-primary">Re-submit your profile </router-link>
-        <p>{{formData.reason}} </p>
+        <h3>{{formData.reason}} </h3>
+        <router-link to="/apply-for-review" class="btn btn-primary">Re-submit your profile </router-link>        
     </div>
 </template>
 
