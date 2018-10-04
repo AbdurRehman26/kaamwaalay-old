cb476f46a0bcd47ffa6fba6568aa2df3152054ac
Kazmi <abdur.rehman@startrum.com>
2018-10-04 11:53:03 +0500

    testing required on QA

--- resources/assets/js/components/front/common-components/GoogleMap.vue
+++ resources/assets/js/components/front/common-components/GoogleMap.vue
@@ -41,32 +41,51 @@
                 place: null,
             }
         },
-        description: 'Autocomplete Example (#164)',
-        methods: {
-            setPlace(place) {
-                this.place = place
-            },
-            setDescription(description) {
-                this.description = description;
-            },
-            usePlace(place) {
-                if (this.place) {
-                    var newPostion = {
-                      lat: this.place.geometry.location.lat(),
-                      lng: this.place.geometry.location.lng(),
-                  };
-                  this.center = newPostion;
-                  this.markers[0].position =  newPostion;
-                  this.place = null;
-              }
-          },
-          updateMaker: function(event) {
-              console.log('updateMaker, ', event.latLng.lat());
-              this.markers[0].position = {
-                lat: event.latLng.lat(),
-                lng: event.latLng.lng(),
-            }
+        mounted (){
+         this.geoLocation();
+     },
+     description: 'Autocomplete Example (#164)',
+     methods: {
+        geoLocation(){
+            let self = this;
+            navigator.geolocation.getCurrentPosition((position) => {
+                this.center = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+
+                self.markers[0] = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+
+
+            });
         },
-    }
+        setPlace(place) {
+            this.place = place
+        },
+        setDescription(description) {
+            this.description = description;
+        },
+        usePlace(place) {
+            if (this.place) {
+                var newPostion = {
+                  lat: this.place.geometry.location.lat(),
+                  lng: this.place.geometry.location.lng(),
+              };
+              this.center = newPostion;
+              this.markers[0].position =  newPostion;
+              this.place = null;
+          }
+      },
+      updateMaker: function(event) {
+          console.log('updateMaker, ', event.latLng.lat());
+          this.markers[0].position = {
+            lat: event.latLng.lat(),
+            lng: event.latLng.lng(),
+        }
+    },
+}
 }
 </script>
