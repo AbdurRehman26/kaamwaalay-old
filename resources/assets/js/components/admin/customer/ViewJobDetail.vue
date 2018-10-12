43b79411c59928596f05541052cb8e3f13e48e52
Kazmi <abdur.rehman@startrum.com>
2018-10-12 12:59:31 +0500

    loading issue fixed hope so

--- resources/assets/js/components/admin/customer/ViewJobDetail.vue
+++ resources/assets/js/components/admin/customer/ViewJobDetail.vue
@@ -88,6 +88,7 @@
             },
             getRecords(response){
                 let self = this;
+                console.log(self.loading , 1111);
                 if(response.data){
                     self.records = response.data;
                     self.loading = false;
