0328372f868c52d28de27319d6befa83d2231be7
Ali Kamal <ali.kamal@cygnismedia.com>
2018-10-15 15:33:42 +0500

    zipcode migration done

--- app/Data/Repositories/ZipCodeRepository.php
+++ app/Data/Repositories/ZipCodeRepository.php
@@ -39,22 +39,18 @@ class ZipCodeRepository extends AbstractRepository implements RepositoryContract
 
      public function findByAll($pagination = false,$perPage = 10, $data = [])
     {       
-
-        $this->builder = $this->model;
         if (!empty($data['zip_code'])) {
             $this->builder = $this->builder
                                 ->select('zip_code as id')
                                 ->where('zip_code', 'LIKE', '%'.$data['zip_code'].'%');
         }
-
         return parent::findByAll($pagination, $perPage, $data);
     }
 
     public function findById($id, $refresh = false, $details = false, $encode = true) {
         $data = $this->cache()->get($this->_cacheKey.$id);
-
         if ($data == NULL || $refresh == true) {
-            $query = $this->model->where('zip_code','=', $id)->first();
+            $query = $this->model->where('id','=', $id)->first();
             if ($query != NULL) {
 
                 $data = new \stdClass;
