c3cbb0d65c1acefce31a7965cd80ca56c63551ac
Ali Kamal <ali.kamal@cygnismedia.com>
2018-11-15 17:16:42 +0500

    fixed features service url

--- app/Data/Models/Service.php
+++ app/Data/Models/Service.php
@@ -41,4 +41,20 @@ class Service extends Model
         }
         return $parseImage ? $parseImage : null;       
     }
+    public function getIconAttribute($value)
+    {
+        $parseImage = json_decode($value);
+
+        if(!empty($parseImage[0]->name)) {
+
+            if(substr($parseImage[0]->name, 0, 8) == "https://") {
+                $parseImage[0]->upload_url = $parseImage[0]->name;
+                return  $parseImage;
+            }
+
+            $parseImage[0]->upload_url = Storage::url(config('uploads.service.folder').'/'.$parseImage[0]->name);
+
+        }
+        return $parseImage ? $parseImage : null;       
+    }
 }
