323aa64ca3acebb30fc627cdf755b363f53b88af
Kazmi <abdur.rehman@startrum.com>
2018-12-12 11:02:42 +0500

    invite link view visible

--- resources/assets/js/components/front/jobs/job-detail.vue
+++ resources/assets/js/components/front/jobs/job-detail.vue
@@ -165,7 +165,7 @@
 
                             <div class="no-photos" v-if="isMyJob && jobBids.pagination && !jobBids.pagination.total"> 
                                 No bid(s) available
-                                <router-link v-if="jobCancelled" href="javascript:void(0);" class="pull-right" 
+                                <router-link v-if="record.status !== 'cancelled'" href="javascript:void(0);" class="pull-right" 
                                 :to="{name: 'Explore_Detail' ,  params : { serviceName: record.service.url_suffix , zip : zipCode }}">Invite service providers</router-link>
 
                             </div>
